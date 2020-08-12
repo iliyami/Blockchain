69981d5201f0aafa29e3e42e38cec8f4b0af44b5
Iliya.mi <iliya.mi78@gmail.com>
2020-08-12 14:52:34 +0430

    Blockchain header first release + Genesis block Addedd

---
+++ Blockchain.h
@@ -0,0 +1,29 @@
+#ifndef Blockchain_H
+#define Blockchain_H
+#include "Block.h"
+#include <vector>
+
+class Blockchain
+{
+private:
+    vector <Block> BlockChain;
+public:
+    Blockchain();
+    void ShowBlockchain();
+    void ShowBlock(int);
+    bool POW();
+    void AddBlock();
+
+};
+#endif
+
+Blockchain::Blockchain()
+{
+    Transaction GenesisTrans("", "", 0.00);
+    Block Genesis(GenesisTrans, 0, 0);
+    Genesis.set_nonce(0);
+    BlockChain.push_back(Genesis);
+}
+
+
+
