bd39a3c5b00606a62beeb8f71573bb8df76ab4a2
Iliya.mi <iliya.mi78@gmail.com>
2020-08-15 13:21:58 +0430

    System Added

--- main.cpp
+++ main.cpp
@@ -4,9 +4,56 @@ int main()
 {
     Blockchain BlockChain;
 
-    Transaction T1("Alice", "Bob", 0.0000007);
+    while (true)
+    {
+        cout << "Transfering Money:  1" << endl;
+        cout << "See a block data:   2" << endl;
+        cout << "See The Blockchain: 3" << endl;
+        cout << "Close the program:  4" << endl;
+        cout << "Choose a number:" << endl;
+        char Ans;
+        cin >> Ans;
+        if (Ans == '1')
+        {
+            string SenderKey, ReceiverKey;
+            double Amount;
+            int Diff;
+            cout <<  "Enter Your SenderKey:" << endl;
+            cin >> SenderKey;
+            cout <<  "Enter Your ReceiverKey:" << endl;
+            cin >> ReceiverKey;
+            cout <<  "Enter The Amount:" << endl;
+            cin >> Amount;
+            cout <<  "Enter Your Difficulty:" << endl;
+            cin >> Diff;
 
-    Block block(T1, BlockChain.get_BlockChain()[BlockChain.get_BlockChain().size() - 1].get_PrevHash(), BlockChain.get_BlockChain().size(), 2);
-    
-    BlockChain.AddBlock(block);
+            Transaction T1(SenderKey, ReceiverKey, Amount);
+            
+            Block block(T1, BlockChain.get_BlockChain()[BlockChain.get_BlockChain().size() - 1].get_PrevHash(), BlockChain.get_BlockChain().size(), Diff);
+
+            BlockChain.AddBlock(block);
+
+            BlockChain.CheckHash(block.get_index() - 1, block.get_PrevHash());
+        }
+        else
+        {
+            switch (Ans)
+            {
+            case '2':
+                cout << "Enter your index:" << endl;
+                int index;
+                cin >> index;
+                BlockChain.ShowBlock(index);
+                break;
+            case '3':
+                cout << "Sakhtan" << endl;
+                break;
+            case '4':
+                break;
+            default:
+                cout << "Wrong Choice!" << endl;
+                break;
+            }
+        }
+    }
 }
